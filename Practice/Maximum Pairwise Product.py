62162f82b358de809da467287f2853348e46eeec
Rupanshu Kapoor <rupanshukapoor@outlook.com>
2023-12-30 14:38:19 +0530

    Impleted a faster approach approach to find 2 max numbers from array

--- Practice/Maximum Pairwise Product.py
+++ Practice/Maximum Pairwise Product.py
@@ -53,7 +53,23 @@ class Solution:
                     max2 = nums[j]
             return (max1-1) * (max2-1)
 
+    def maxProductSearching2(self, nums:list[int]) -> int:
+        if len(nums) < 1:
+            return -1
+        if len(nums) == 2:
+            return (nums[0]-1) * (nums[1]-1)
+        else:
+            max1 = max2 = 0
+            for num in nums:
+                if num > max1:
+                    max2 = max1
+                    max1 = num
+                elif num > max2:
+                    max2 = num
+            return (max1-1)*(max2-1)
 
-nums = list(map(int, input("Enter an array: ").split()))
-print(f"Product of two maximum number sorting (i-1) and (j-1) is: {Solution().maxProductSorting(nums)}")
+nums = [10,2,5,2]
+# nums = list(map(int, input("Enter an array: ").split()))
+# print(f"Product of two maximum number sorting (i-1) and (j-1) is: {Solution().maxProductSorting(nums)}")
 print(f"Product of two maximum number searching (i-1) and (j-1) is: {Solution().maxProductSearching(nums)}")
+print(f"Product of two maximum number searching2 (i-1) and (j-1) is: {Solution().maxProductSearching2(nums)}")
