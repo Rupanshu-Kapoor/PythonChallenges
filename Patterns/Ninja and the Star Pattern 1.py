55cf4954376728ffce163e83901db7d0bfc86242
Rupanshu Kapoor <rupanshukapoor@outlook.com>
2023-12-30 14:28:06 +0530

    Ninja and the star pattern

---
+++ Patterns/Ninja and the Star Pattern 1.py
@@ -0,0 +1,29 @@
+"""
+    date: 29-12-2023
+    Ninja and the Star Pattern1 : https://www.codingninjas.com/studio/problems/ninja-and-the-star-pattern-i_6581920
+
+    Runtime: Better than 100% users :*
+
+Input: ‘N’ = 4
+
+Output:
+            ****
+            *  *
+            *  *
+            ****
+"""
+
+
+#
+def getStarPattern(n: int) -> None:
+
+    for i in range(n):
+        if i == 0 or i == n-1:
+            string = n*"*"
+        else:
+            string = "*" + (n-2)*" "+"*"
+        print(string)
+
+
+num = int(input("Enter \"N\" to print N*N Star Pattern: "))
+getStarPattern(num)
