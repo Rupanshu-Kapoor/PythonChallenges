1b8c800eda8414294b9b910bd3ec43b78b118a93
Rupanshu Kapoor <rupanshukapoor@outlook.com>
2024-01-18 01:03:33 +0530

    Chore: Striver's DSA Sheet: Leetcode: single Number

---
+++ DSA/Striver's Sheet/arrays/easy/singleNumber.py
@@ -0,0 +1,47 @@
+"""
+Single Number
+https://leetcode.com/problems/single-number/description/
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+Example 1:
+
+Input: nums = [2,2,1]
+Output: 1
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+Output: 4
+Example 3:
+
+Input: nums = [1]
+Output: 1
+
+Constraints:
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
+
+"""
+
+
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        occur = {}
+
+        for i in nums:
+            occur[i] = occur.get(i, 0) + 1
+        for i in occur:
+            if occur[i] == 1:
+                return i
+
+    def singleNumberOP(self, nums: list[int]) -> int:
+        xor = 0
+        for i in nums:
+            xor ^= i
+        return xor
+
+
+n = [2, 2, 1, 1, 5]
+print(Solution().singleNumber(n))
+print(Solution().singleNumberOP(n))
