6a117ce2c25dc58c061cd7a69a7789fd97cfa202
Rupanshu Kapoor <rupanshukapoor@outlook.com>
2024-03-11 18:45:10 +0530

    Striver DSA Sheet: Longest Subarray

---
+++ DSA/Coursera/Course1- Alorithm Toolbox/greedySol.py
@@ -0,0 +1,16 @@
+# def maxSalary(arr: list[int]) -> int:
+#     temp = 0
+#     result = ""
+#     for i in range(len(arr)):
+#         for j in arr:
+#             if j > temp:
+#                 temp = j
+#         arr.remove(temp)
+#         result += str(temp)
+#         temp = 0
+#     return result
+#
+#
+# max = maxSalary([8, 5, 3, 5, 6, 7])
+# print(max)
+#
